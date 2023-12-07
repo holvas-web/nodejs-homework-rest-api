0aec0dbf4a663d0ca20ed16c03e76d25de899690
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-12-07 15:50:47 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,6 +14,7 @@ const { authSchema, subscriptionSchema, verifySchema } = require("../routes/sche
 
 const SECRET_KEY = process.env.SECRET_KEY;
 const FROM_EMAIL = process.env.USER_EMAIL;
+const BASE_URL = process.env.BASE_URL;
 
 const defaultClient = ElasticEmail.ApiClient.instance;
 const { apikey } = defaultClient.authentications;
